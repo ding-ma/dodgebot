8cc57392d6f52991ea05e9b03dde17564613dc7f
Ding Ma <ding.ma@mail.mcgill.ca>
2020-09-30 15:55:40 -0400

    web app and CI/CD works

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cover from "./Images/League_Cover.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cover}alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hey, this site is still under construction. Please come back later :)!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
