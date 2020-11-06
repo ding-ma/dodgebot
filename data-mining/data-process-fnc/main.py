6875e4ff2547873854c83f02fadad956ae7be524
ding-ma <ding.ma@mail.mcgill.ca>
2020-11-06 09:45:32 -0500

    should work

--- data-mining/data-process-fnc/main.py
+++ data-mining/data-process-fnc/main.py
@@ -4,6 +4,8 @@ import pandas as pd
 # from dotenv import load_dotenv
 # load_dotenv(".env.na1")  # loads the env file for local development
 
+MAX_SIZE = 70000
+
 
 def set_blob_metadata(blob_name, bucket_name="dodge-bot"):
     """Set a blob's metadata."""
@@ -36,12 +38,14 @@ def resize_and_clean(event, context):
 
     print('Bucket: {}'.format(event['bucket']))
     print('File: {}'.format(event['name']))
+    base_path = "/tmp/"
 
     client = storage.Client()
     upload_bucket = client.get_bucket(event['bucket'])
     uploaded_blob = upload_bucket.get_blob(event['name'])
     uploaded_file_name = uploaded_blob.name.split("/")[-1]
-    uploaded_blob.download_to_filename("/tmp/"+uploaded_file_name)
+    uploaded_blob.download_to_filename(base_path + uploaded_file_name)
+
     paths = uploaded_blob.name.split("/")[:-1]
     region = paths[0]
     tier = paths[1]
@@ -49,11 +53,20 @@ def resize_and_clean(event, context):
     remainder_bucket = client.get_bucket("dodge-bot-remainder")
     remainder_file_name = '{}-{}.csv'.format(region, tier)
     remainder_blob = remainder_bucket.get_blob(remainder_file_name)
-    remainder_blob.download_to_filename("/tmp/"+remainder_file_name)
+    remainder_blob.download_to_filename(base_path + remainder_file_name)
 
-    uploaded_df = pd.read_csv("/tmp/"+uploaded_file_name)
-    print(uploaded_df.describe())
+    uploaded_df = pd.read_csv(base_path + uploaded_file_name)
+    print(uploaded_file_name, uploaded_df.shape)
 
-    remainder_df = pd.read_csv("/tmp/"+remainder_file_name)
-    print(remainder_df.describe())
+    remainder_df = pd.read_csv(base_path + remainder_file_name)
+    print(remainder_file_name, remainder_df.shape)
 
+    uploaded_df.drop_duplicates(subset="GAME_ID", inplace=True)
+    combined_df = pd.concat([uploaded_df, remainder_df])
+    print("combined", combined_df.shape)
+
+    if combined_df.shape[0] > MAX_SIZE:
+        df_70k = combined_df[:MAX_SIZE]
+        df_remainder_split = combined_df[MAX_SIZE:]
+        print(df_70k.shape)
+        print(df_remainder_split.shape)
