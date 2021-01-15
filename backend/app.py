d58f0653da175dad684a388a17edb33eaccdee50
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2021-01-14 21:57:01 -0500

    mock backend done

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,75 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from keras.models import load_model
 import tensorflow as tf
 import os
+from time import sleep
+import random
 
+"""
+input example:
+{
+    "blue":{
+        "picks":{
+            "top":"aatrox",
+            "mid":"mundo",
+            "jg":"yi",
+            "adc":"kaisa",
+            "supp":"thresh"
+        },
+        "bans":[
+            "ahri", "akali", "bob", "-1", "aaa"
+        ]
+    },
+    "red":{
+        "picks":{
+            "top":"aatrox",
+            "mid":"mundo",
+            "jg":"yi",
+            "adc":"kaisa",
+            "supp":"thresh"
+        },
+        "bans":[
+            "ahri", "akali", "bob", "-1", "aaa"
+        ]
+    }
+}
+
+output example:
+{
+"blue: 0.55,
+"red": 0.45
+}
+"""
 app = Flask(__name__)
 
+
 # Load model from file
-model = tf.keras.models.load_model(os.path.join("models", "model.h5"))
+
+def mock_predict():
+    n = random.random()
+    return {"red": n, "blue": 1 - n}
+
 
 # Predicts winner of match
-@app.route('/predictWinner')
+@app.route('/predictwinner')
 def predict_winner():
     data = request.get_json()
+    print(data)
     
     # Create array of champions from request data
     champions = create_champion_array(data)
-
+    sleep(random.randrange(1, 10))
     # Make prediction
-    prediction = model.predict([champions])
-    return str(prediction[0][0])
+    prediction = mock_predict()
+    
+    return mock_predict()
+
 
 # Creates champion array from json data
 def create_champion_array(data):
-    return [data["redTop"], data["redJungle"], data["redMid"], data["redAdc"], data["redSupport"], data["blueTop"],
-            data["blueJungle"], data["blueMid"], data["blueAdc"], data["blueSupport"]]
+    # TODO
+    return []
+
 
 # TODO: change flask to production WSGI server
 if __name__ == "__main__":
