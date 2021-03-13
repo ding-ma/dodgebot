90f5835c9e9e6237e7ea5bd9df97c0d8e9342191
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2021-03-13 17:18:09 -0500

    implemented ci for backend

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,39 @@
 from flask import Flask
 from flask import request
-from keras.models import load_model
 import tensorflow as tf
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Load model from file
 model = tf.keras.models.load_model(os.path.join("models", "model.h5"))
 
+
+@app.route("/")
+def test():
+    return "!!!!!", 200
+
+
 # Predicts winner of match
-@app.route('/predictWinner')
+@app.route('/predictWinner', methods=["POST"])
 def predict_winner():
     data = request.get_json()
     
     # Create array of champions from request data
     champions = create_champion_array(data)
-
+    
     # Make prediction
     prediction = model.predict([champions])
     return str(prediction[0][0])
+
 
 # Creates champion array from json data
 def create_champion_array(data):
     return [data["redTop"], data["redJungle"], data["redMid"], data["redAdc"], data["redSupport"], data["blueTop"],
             data["blueJungle"], data["blueMid"], data["blueAdc"], data["blueSupport"]]
 
-# TODO: change flask to production WSGI server
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(threaded=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
