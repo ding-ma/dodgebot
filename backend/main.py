0432c30e9af353a51189bbcce4dcc2ca4b94c96b
Petar Basta <petar.basta@hotmail.com>
2021-03-13 17:44:51 -0500

    Add cors, attach predictions frontend to backend

--- backend/main.py
+++ backend/main.py
@@ -3,31 +3,39 @@ from flask import request
 import tensorflow as tf
 import os
 from flask_cors import CORS
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
 # Load model from file
-model = tf.keras.models.load_model(os.path.join("models", "model.h5"))
+model = tf.keras.models.load_model(os.path.join("models", "Bronze.h5"))
 
 
 # Predicts winner of match
-@app.route('/predictWinner', methods=["POST"])
+@app.route('/predictWinner', methods=['POST'])
 def predict_winner():
+
     data = request.get_json()
     
     # Create array of champions from request data
-    champions = create_champion_array(data)
-    
-    # Make prediction
-    prediction = model.predict([champions])
-    return str(prediction[0][0])
+    championArray = create_champion_array(data)
 
+    predictData = []
+    predictData.append(championArray)
+    predictData = np.array(predictData)
+    predictData = predictData.astype('int32')
+    predictData = [predictData[:, i] for i in range(predictData.shape[1])]
+
+    # Make prediction
+    prediction = model.predict(predictData)
+    return str(prediction[0][0])
 
 # Creates champion array from json data
 def create_champion_array(data):
-    return [data["redTop"], data["redJungle"], data["redMid"], data["redAdc"], data["redSupport"], data["blueTop"],
-            data["blueJungle"], data["blueMid"], data["blueAdc"], data["blueSupport"]]
+    print(data)
+    return [float(data["redTop"]), float(data["redJungle"]), float(data["redMid"]), float(data["redAdc"]), float(data["redSupport"]), float(data["blueTop"]),
+            float(data["blueJungle"]), float(data["blueMid"]), float(data["blueAdc"]), float(data["blueSupport"])]
 
 
 if __name__ == "__main__":
--- frontend/src/pages/Prediction/containers/Predictions.tsx
+++ frontend/src/pages/Prediction/containers/Predictions.tsx
@@ -6,7 +6,7 @@ import OuterOval from "../../../images/Outer Oval.png"
 import OvalInside from "../../../images/Oval Inside.png"
 import PredictBox from "../../../images/Predict Box.png"
 import PredictBoxLeft from "../../../images/Predict Box Side.png"
-import {ChampToKey} from "../../../constants/ChampToKey"
+import { ChampToKey } from "../../../constants/ChampToKey"
 
 import '../styles/Predictions.css'
 import ChampionScroll from "../components/ChampionScroll";
@@ -15,271 +15,284 @@ var selectingTeam: string | null = null
 var selectingRole: number | null = null
 
 type PredictionsState = {
-    friendlyTeam: string[]
-    enemyTeam: string[]
-    submitted: Boolean
-    isLoading: Boolean
-    winPercentage: number | null
+  friendlyTeam: string[]
+  enemyTeam: string[]
+  submitted: Boolean
+  isLoading: Boolean
+  winPercentage: number | null
 }
 
 class Predictions extends React.Component<{}, PredictionsState> {
-    private el: any;
-    
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            friendlyTeam: ["", "", "", "", ""],
-            enemyTeam: ["", "", "", "", ""],
-            submitted: false,
-            isLoading: false,
-            winPercentage: null
-        }
-        
-        this.selectChamp = this.selectChamp.bind(this)
-        this.predict = this.predict.bind(this)
-        this.reset = this.reset.bind(this)
+  private el: any;
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      friendlyTeam: ["", "", "", "", ""],
+      enemyTeam: ["", "", "", "", ""],
+      submitted: false,
+      isLoading: false,
+      winPercentage: null
     }
-    
-    componentDidMount() {
-        this.scrollToBottom();
+
+    this.selectChamp = this.selectChamp.bind(this)
+    this.predict = this.predict.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    this.el.scrollIntoView({ behavior: 'auto' });
+    document.body.style.overflow = 'hidden';
+  }
+
+  // When selecting friendly champ
+  selectingFriendly(number: number) {
+    selectingTeam = "friendly"
+    selectingRole = number
+  }
+
+  // When selecting enemy champ
+  selectingEnemy(number: number) {
+    selectingTeam = "enemy"
+    selectingRole = number
+  }
+
+  // Chooses champ
+  selectChamp(champ: string) {
+
+    let temp;
+    if (ChampToKey[champ as keyof typeof ChampToKey] === undefined) {
+      console.log(champ)
     }
-    
-    componentDidUpdate() {
-        this.scrollToBottom();
-    }
-    
-    scrollToBottom() {
-        this.el.scrollIntoView({ behavior: 'auto' });
-        document.body.style.overflow = 'hidden';
-    }
-    
-    // When selecting friendly champ
-    selectingFriendly(number: number) {
-        selectingTeam = "friendly"
-        selectingRole = number
-    }
-    
-    // When selecting enemy champ
-    selectingEnemy(number: number) {
-        selectingTeam = "enemy"
-        selectingRole = number
-    }
-    
-    // Chooses champ
-    selectChamp(champ: string) {
-        let temp;
-        if (ChampToKey[champ as keyof typeof ChampToKey] === undefined) {
-            console.log(champ)
-        }
-        
-        if (selectingTeam != null && selectingRole != null) {
-            if (selectingTeam === "friendly") {
-                temp = this.state.friendlyTeam;
-                temp[selectingRole] = champ
-                this.setState({
-                    friendlyTeam: temp
-                })
-            } else {
-                temp = this.state.enemyTeam;
-                temp[selectingRole] = champ
-                this.setState({
-                    enemyTeam: temp
-                })
-            }
-        }
-    }
-    
-    // Predict Winner
-    predict() {
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Headers": "*"
-            },
-            body: JSON.stringify({title: 'React POST Request Example'})
-        };
-        
+
+    if (selectingTeam != null && selectingRole != null) {
+      if (selectingTeam === "friendly") {
+        temp = this.state.friendlyTeam;
+        temp[selectingRole] = champ
         this.setState({
-            isLoading: true,
-            submitted: true
-        }, () => {
-            fetch('https://us-central1-ordinal-cacao-291815.cloudfunctions.net/predict-function', requestOptions)
-                .then(response => response.json())
-                .then(async data => {
-                    this.setState({
-                        isLoading: false,
-                        winPercentage: data["winPercentage"]
-                    })
-                });
+          friendlyTeam: temp
         })
-    }
-    
-    reset() {
+      } else {
+        temp = this.state.enemyTeam;
+        temp[selectingRole] = champ
         this.setState({
-            friendlyTeam: ["", "", "", "", ""],
-            enemyTeam: ["", "", "", "", ""],
-            submitted: false,
-            isLoading: false,
-            winPercentage: null
+          enemyTeam: temp
         })
+      }
     }
-    
-    render() {
-        var winColor = "white"
-        var winPercentageFormatted = 0
-        if (this.state.winPercentage != null) {
-            winColor = this.state.winPercentage >= 0.5 ? "green" : "red"
-            winPercentageFormatted = Math.round(this.state.winPercentage * 1000) / 10
-        }
-        
-        
-        var resultsPage = this.state.isLoading ?
-            <div style={{
-                zIndex: 5,
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <div className="loader"/>
-            </div>
-            :
-            <div style={{
-                zIndex: 5,
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <p style={{
-                    color: "#7b775b",
-                    textAlign: "center",
-                    fontSize: "30pt",
-                    fontFamily: "Courier New",
-                    margin: 0
-                }}>You have a</p>
-                <p style={{
-                    color: winColor,
-                    textAlign: "center",
-                    fontSize: "100pt",
-                    fontFamily: "Courier New",
-                    margin: 0
-                }}>{winPercentageFormatted}%</p>
-                <p style={{
-                    color: "#7b775b",
-                    textAlign: "center",
-                    fontSize: "30pt",
-                    fontFamily: "Courier New",
-                    margin: 0
-                }}>chance of winning</p>
-                
-                <button className="resetBtn" onClick={this.reset}>Reset</button>
-            </div>
-        
-        return (
-            // Background
-            <div className="backgroundImg">
-                
-                <div style={{display: "flex", justifyContent: "center", margin: "6.5vh"}}>
-                    
-                    {/* Friendly panel */}
-                    <div style={{width: "20%", display: "flex", justifyContent: "center"}}>
-                        <ChampionPanel isLeftSide={true} selectBox={this.selectingFriendly}
-                                       champions={this.state.friendlyTeam}/>
-                    </div>
-                    
-                    {/* Centre panel */}
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        width: "60%",
-                        height: "85vh",
-                        marginTop: "4vh",
-                        position: "relative"
+  }
+
+  // Predict Winner
+  predict() {
+    if (!this.state.friendlyTeam.includes("") && !this.state.enemyTeam.includes("")) {
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify({
+          "redTop":  ChampToKey[this.state.friendlyTeam[0] as keyof typeof ChampToKey],
+          "redJungle": ChampToKey[this.state.friendlyTeam[1] as keyof typeof ChampToKey],
+          "redMid": ChampToKey[this.state.friendlyTeam[2] as keyof typeof ChampToKey],
+          "redAdc": ChampToKey[this.state.friendlyTeam[3] as keyof typeof ChampToKey],
+          "redSupport": ChampToKey[this.state.friendlyTeam[4] as keyof typeof ChampToKey],
+          "blueTop": ChampToKey[this.state.enemyTeam[0] as keyof typeof ChampToKey],
+          "blueJungle": ChampToKey[this.state.enemyTeam[1] as keyof typeof ChampToKey],
+          "blueMid": ChampToKey[this.state.enemyTeam[2] as keyof typeof ChampToKey],
+          "blueAdc": ChampToKey[this.state.enemyTeam[3] as keyof typeof ChampToKey],
+          "blueSupport": ChampToKey[this.state.enemyTeam[4] as keyof typeof ChampToKey],
+        })
+      };
+
+      this.setState({
+        isLoading: true,
+        submitted: true
+      }, () => {
+        fetch('http://127.0.0.1:8080/predictWinner', requestOptions)
+          .then(response => response.json())
+          .then(async data => {
+            console.log(data)
+            this.setState({
+              isLoading: false,
+              winPercentage: data
+            })
+          });
+      })
+    }
+  }
+
+  reset() {
+    this.setState({
+      submitted: false,
+      isLoading: false,
+      winPercentage: null
+    })
+  }
+
+  render() {
+    var winColor = "white"
+    var winPercentageFormatted = 0
+    if (this.state.winPercentage != null) {
+      winColor = this.state.winPercentage >= 0.5 ? "green" : "red"
+      winPercentageFormatted = Math.round(this.state.winPercentage * 1000) / 10
+    }
+
+
+    var resultsPage = this.state.isLoading ?
+      <div style={{
+        zIndex: 5,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <div className="loader" />
+      </div>
+      :
+      <div style={{
+        zIndex: 5,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <p style={{
+          color: "#7b775b",
+          textAlign: "center",
+          fontSize: "30pt",
+          fontFamily: "Courier New",
+          margin: 0
+        }}>You have a</p>
+        <p style={{
+          color: winColor,
+          textAlign: "center",
+          fontSize: "100pt",
+          fontFamily: "Courier New",
+          margin: 0
+        }}>{winPercentageFormatted}%</p>
+        <p style={{
+          color: "#7b775b",
+          textAlign: "center",
+          fontSize: "30pt",
+          fontFamily: "Courier New",
+          margin: 0
+        }}>chance of winning</p>
+
+        <button className="resetBtn" onClick={this.reset}>Reset</button>
+      </div>
+
+    return (
+      // Background
+      <div className="backgroundImg">
+
+        <div style={{ display: "flex", justifyContent: "center", margin: "6.5vh" }}>
+
+          {/* Friendly panel */}
+          <div style={{ width: "20%", display: "flex", justifyContent: "center" }}>
+            <ChampionPanel isLeftSide={true} selectBox={this.selectingFriendly}
+              champions={this.state.friendlyTeam} />
+          </div>
+
+          {/* Centre panel */}
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "60%",
+            height: "85vh",
+            marginTop: "4vh",
+            position: "relative"
+          }}>
+
+            {/* Centre panel background */}
+            <img style={{ zIndex: 4, position: "absolute", width: "90%", height: "100%" }} src={OuterOval}
+              className="centered" alt="" />
+            <img style={{ zIndex: 4, position: "absolute", width: "90%", height: "100%" }} src={InnerOval}
+              className="centered" alt="" />
+            <img style={{ zIndex: 3, position: "absolute", width: "90%", height: "100%" }} src={OvalInside}
+              className="centered" alt="" />
+
+            {this.state.submitted ? resultsPage :
+              (
+                <div style={{ zIndex: 100, width: "100%" }}>
+
+                  {/* Scrollable list of champions with search bar */}
+                  <div>
+                    <ChampionScroll selectChamp={this.selectChamp} />
+                  </div>
+                  <div style={{
+                    height: "9vh",
+                    width: "100%",
+                    display: "flex",
+                    position: "relative",
+                    justifyContent: "center",
+                    alignItems: "flex-end"
+                  }}>
+
+
+                    {/* Predict Button */}
+                    <img style={{ width: "25%", height: "70%" }} src={PredictBox}
+                      onClick={this.predict} alt="" />
+                    <p onClick={this.predict} style={{
+                      zIndex: 100,
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      position: "absolute",
+                      bottom: -18,
+                      color: "#b8bcbd",
+                      fontFamily: 'Garamond',
+                      fontSize: "22pt"
                     }}>
-                        
-                        {/* Centre panel background */}
-                        <img style={{zIndex: 4, position: "absolute", width: "90%", height: "100%"}} src={OuterOval}
-                             className="centered" alt=""/>
-                        <img style={{zIndex: 4, position: "absolute", width: "90%", height: "100%"}} src={InnerOval}
-                             className="centered" alt=""/>
-                        <img style={{zIndex: 3, position: "absolute", width: "90%", height: "100%"}} src={OvalInside}
-                             className="centered" alt=""/>
-                        
-                        {this.state.submitted ? resultsPage :
-                            (
-                                <div style={{zIndex: 100, width: "100%"}}>
-                                    
-                                    {/* Scrollable list of champions with search bar */}
-                                    <div>
-                                        <ChampionScroll selectChamp={this.selectChamp}/>
-                                    </div>
-                                    <div style={{
-                                        height: "9vh",
-                                        width: "100%",
-                                        display: "flex",
-                                        position: "relative",
-                                        justifyContent: "center",
-                                        alignItems: "flex-end"
-                                    }}>
-                                        
-                                        
-                                        {/* Predict Button */}
-                                        <img style={{width: "25%", height: "70%"}} src={PredictBox}
-                                             onClick={this.predict} alt=""/>
-                                        <p onClick={this.predict} style={{
-                                            zIndex: 100,
-                                            fontWeight: "bold",
-                                            textAlign: "center",
-                                            position: "absolute",
-                                            bottom: -18,
-                                            color: "#b8bcbd",
-                                            fontFamily: 'Garamond',
-                                            fontSize: "22pt"
-                                        }}>
-                                            PREDICT
+                      PREDICT
                                         </p>
-                                        
-                                        <img style={{
-                                            position: "absolute",
-                                            height: "50%",
-                                            width: "4.35%",
-                                            bottom: "18%",
-                                            left: "35.2%"
-                                        }} src={PredictBoxLeft} alt=""/>
-                                        <img style={{
-                                            position: "absolute",
-                                            height: "50%",
-                                            width: "4.3%",
-                                            bottom: "18%",
-                                            left: "60.5%",
-                                            transform: "scaleX(-1)"
-                                        }} src={PredictBoxLeft} alt=""/>
-                                    
-                                    </div>
-                                </div>
-                            )
-                        }
-                    
-                    </div>
-                    
-                    {/* Enemy Panel */}
-                    <div style={{width: "20%", display: "flex", justifyContent: "center"}}>
-                        <ChampionPanel isLeftSide={false} selectBox={this.selectingEnemy}
-                                       champions={this.state.enemyTeam}/>
-                    </div>
+
+                    <img style={{
+                      position: "absolute",
+                      height: "50%",
+                      width: "4.35%",
+                      bottom: "18%",
+                      left: "35.2%"
+                    }} src={PredictBoxLeft} alt="" />
+                    <img style={{
+                      position: "absolute",
+                      height: "50%",
+                      width: "4.3%",
+                      bottom: "18%",
+                      left: "60.5%",
+                      transform: "scaleX(-1)"
+                    }} src={PredictBoxLeft} alt="" />
+
+                  </div>
                 </div>
-                <div ref={el => { this.el = el; }} />
-            </div>
-        );
-    };
+              )
+            }
+
+          </div>
+
+          {/* Enemy Panel */}
+          <div style={{ width: "20%", display: "flex", justifyContent: "center" }}>
+            <ChampionPanel isLeftSide={false} selectBox={this.selectingEnemy}
+              champions={this.state.enemyTeam} />
+          </div>
+        </div>
+        <div ref={el => { this.el = el; }} />
+      </div>
+    );
+  };
 }
 
 export default Predictions;
