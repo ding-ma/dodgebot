4e309e362f8be6d35d26b4c843b601e54d39407a
Petar Basta <petar.basta@hotmail.com>
2021-03-18 18:11:26 -0400

    Change ADC to Bot

--- frontend/src/pages/Prediction/components/ChampionPanel.tsx
+++ frontend/src/pages/Prediction/components/ChampionPanel.tsx
@@ -18,7 +18,7 @@ class ChampionPanel extends React.Component<ChampionPanelProps, {}> {
                        champion={this.props.champions[1]}/>
                 <Input isFriendlyTeam={this.props.isFriendlyTeam} role="Mid" onClick={() => this.props.selectBox(2)}
                        champion={this.props.champions[2]}/>
-                <Input isFriendlyTeam={this.props.isFriendlyTeam} role="ADC" onClick={() => this.props.selectBox(3)}
+                <Input isFriendlyTeam={this.props.isFriendlyTeam} role="Bot" onClick={() => this.props.selectBox(3)}
                        champion={this.props.champions[3]}/>
                 <Input isFriendlyTeam={this.props.isFriendlyTeam} role="Support" onClick={() => this.props.selectBox(4)}
                        champion={this.props.champions[4]}/>
