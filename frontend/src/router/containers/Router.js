a34d6751282370004e8a7bff6a8826445c86798c
Petar Basta <petar.basta@hotmail.com>
2021-03-24 12:56:34 -0400

    Update mobile display

--- frontend/src/router/containers/Router.js
+++ frontend/src/router/containers/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '../../global/containers/Layout';
 import Stats from "../../pages/Stats/containers/Stats";
 import Login from "../../pages/Login/containers/Login"
@@ -11,13 +11,13 @@ import Dashboard from "../../pages/History";
 import PrivateRoute from "../components/PrivateRoute";
 import ResetPasswordCallBack from '../../pages/ResetPassword/containers/ResetPwdCallBack';
 import firebase from "firebase";
-import {AuthProvider} from "../../context/providers/AccountProvider";
+import { AuthProvider } from "../../context/providers/AccountProvider";
 import Header from "../../global/components/Header";
 import Setting from "../../pages/Setting/containers/Setting";
 import Predictions from "../../pages/Prediction/containers/Predictions";
 import Favorites from "../../pages/Favorites/containers/Favorites";
 import ReactNotification from 'react-notifications-component'
-import {isMobile} from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 
 
 const Router = () => {
@@ -34,27 +34,29 @@ const Router = () => {
     }
 
     if (isMobile) {
-        return <div>DodgeBot is not available on mobile yet. Please access it on your computer</div>
+        return <div>
+            <p style={{textAlign: "center", color: "#87742a", marginTop: "30vh", marginLeft: "5%", marginRight: "5%"}}>DodgeBot is not available on mobile yet. Please access it on your computer.</p>
+        </div >
     }
 
     return (
         <AuthProvider>
-            <ReactNotification/>
-            <Header/>
+            <ReactNotification />
+            <Header />
             <BrowserRouter>
                 <Layout>
                     <Switch>
-                        <Route exact path="/" component={Login}/>
-                        <Route exact path="/stats" component={Stats}/>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/reset" component={ResetPwd}/>
-                        <Route exact path="/confirm" component={ResetPasswordCallBack}/>
-                        <PrivateRoute exact path="/new" component={NewPlayer}/>
-                        <PrivateRoute exact path="/history" component={Dashboard}/>
-                        <PrivateRoute exact path="/settings" component={Setting}/>
-                        <PrivateRoute exact path="/predict" component={Predictions}/>
-                        <PrivateRoute exact path="/favorites" component={Favorites}/>
-                        <Route component={NotFound}/>
+                        <Route exact path="/" component={Login} />
+                        <Route exact path="/stats" component={Stats} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/reset" component={ResetPwd} />
+                        <Route exact path="/confirm" component={ResetPasswordCallBack} />
+                        <PrivateRoute exact path="/new" component={NewPlayer} />
+                        <PrivateRoute exact path="/history" component={Dashboard} />
+                        <PrivateRoute exact path="/settings" component={Setting} />
+                        <PrivateRoute exact path="/predict" component={Predictions} />
+                        <PrivateRoute exact path="/favorites" component={Favorites} />
+                        <Route component={NotFound} />
                     </Switch>
                 </Layout>
             </BrowserRouter>
