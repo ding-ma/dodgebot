4f236510418a4454dacdf67e18a729560465a8ed
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2021-03-29 13:35:37 -0400

    added x label and fixed table

--- frontend/src/pages/Favorites/components/ChampionTable.js
+++ frontend/src/pages/Favorites/components/ChampionTable.js
@@ -1,160 +1,157 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {DataGrid} from '@material-ui/data-grid';
-import {KeyToChamp} from "../../../constants/KeyToChampion";
-import {ChampToKey} from "../../../constants/ChampToKey";
-import {Button} from "@material-ui/core";
+import React, { useContext, useEffect, useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { KeyToChamp } from "../../../constants/KeyToChampion";
+import { ChampToKey } from "../../../constants/ChampToKey";
+import { Button } from "@material-ui/core";
 import firebase from "firebase";
-import {AuthContext} from "../../../context/providers/AccountProvider";
-import {store} from 'react-notifications-component';
-import "../styles/table.scss"
+import { AuthContext } from "../../../context/providers/AccountProvider";
+import { store } from "react-notifications-component";
+import "../styles/table.scss";
 
 const columns = [
-    {field: 'champion', headerName: 'Champion Name', width: 200},
-    {field: 'role', headerName: 'Role', width: 130},
+  { field: "champion", headerName: "CHAMPION NAME", width: 200 },
+  { field: "role", headerName: "ROLE", width: 130 },
 ];
 
 function isEmpty(obj) {
-    return Object.keys(obj).length === 0;
+  return Object.keys(obj).length === 0;
 }
 
 const displayRoleToDb = {
-    "Top Lane": "top",
-    "Jungle": "jg",
-    "Mid Lane": "mid",
-    "Bot Lane": "adc",
-    "Support": "sup"
-}
+  "Top Lane": "top",
+  Jungle: "jg",
+  "Mid Lane": "mid",
+  "Bot Lane": "adc",
+  Support: "sup",
+};
 
 let counter = 1;
 
-export default function ChampionTable({favoriteChampions}) {
+export default function ChampionTable({ favoriteChampions }) {
+  const { currentUser } = useContext(AuthContext);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [data, setData] = useState([]);
+  const flatten = (arr, name) => {
+    let tmp = [];
+    for (let i = 0; i < arr.length; i++) {
+      tmp.push({ id: counter, champion: KeyToChamp[arr[i]], role: name });
+      counter++;
+    }
+    return tmp;
+  };
 
-    const {currentUser} = useContext(AuthContext);
-    const [selectedItems, setSelectedItems] = useState([])
-    const [data, setData] = useState([])
-    const flatten = (arr, name) => {
-        let tmp = []
-        for (let i = 0; i < arr.length; i++) {
-            tmp.push({'id': counter, 'champion': KeyToChamp[arr[i]], 'role': name})
-            counter++;
+  useEffect(() => {
+    if (favoriteChampions !== undefined && !isEmpty(favoriteChampions)) {
+      const { jg, top, adc, sup, mid } = favoriteChampions;
+      setData([
+        ...flatten(top, "Top Lane"),
+        ...flatten(jg, "Jungle"),
+        ...flatten(adc, "Bot Lane"),
+        ...flatten(mid, "Mid Lane"),
+        ...flatten(sup, "Support"),
+      ]);
+    }
+  }, [favoriteChampions]);
+
+  const selectItems = (selected) => {
+    let tmp = [];
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < selected.length; j++) {
+        if (data[i].id === parseInt(selected[j])) {
+          tmp.push(data[i]);
         }
-        return tmp
+      }
+    }
+    setSelectedItems(tmp);
+  };
+
+  const handleDelete = async () => {
+    console.log(selectedItems);
+    if (selectedItems.length === 0) {
+      store.addNotification({
+        title: "You haven't selected anything!",
+        message: "Nothing was deleted",
+        type: "warning",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+        },
+      });
+      return;
     }
 
-    useEffect(() => {
-        if (favoriteChampions !== undefined && !isEmpty(favoriteChampions)) {
-            const {jg, top, adc, sup, mid} = favoriteChampions
-            setData([
-                ...flatten(top, 'Top Lane'),
-                ...flatten(jg, 'Jungle'),
-                ...flatten(adc, 'Bot Lane'),
-                ...flatten(mid, 'Mid Lane'),
-                ...flatten(sup, 'Support'),
-            ])
-        }
-    }, [favoriteChampions])
+    const championsToDelete = selectedItems.map((e) => {
+      return {
+        champion: ChampToKey[e.champion],
+        role: displayRoleToDb[e.role],
+      };
+    });
 
-
-    const selectItems = (selected) => {
-        let tmp = []
-        for (let i = 0; i < data.length; i++) {
-            for (let j = 0; j < selected.length; j++) {
-                if (data[i].id === parseInt(selected[j])) {
-                    tmp.push(data[i])
-                }
-            }
-        }
-        setSelectedItems(tmp)
-    }
-
-    const handleDelete = async () => {
-        console.log(selectedItems)
-        if(selectedItems.length === 0){
-            store.addNotification({
-                title: "You haven't selected anything!",
-                message: "Nothing was deleted",
-                type: "warning",
-                insert: "top",
-                container: "top-right",
-                animationIn: ["animate__animated", "animate__fadeIn"],
-                animationOut: ["animate__animated", "animate__fadeOut"],
-                dismiss: {
-                    duration: 5000
-                }
+    let deleted = {};
+    for (let key in favoriteChampions) {
+      if (favoriteChampions.hasOwnProperty(key)) {
+        for (let i in championsToDelete) {
+          if (championsToDelete[i].role === key) {
+            deleted[key] = favoriteChampions[key].filter((e) => {
+              return e !== championsToDelete[i].champion;
             });
-            return;
+          }
         }
-
-
-        const championsToDelete = selectedItems.map(e => {
-            return {"champion": ChampToKey[e.champion], "role": displayRoleToDb[e.role]}
-        })
-
-        let deleted = {}
-        for (let key in favoriteChampions) {
-            if (favoriteChampions.hasOwnProperty(key)) {
-                for (let i in championsToDelete) {
-                    if (championsToDelete[i].role === key) {
-                        deleted[key] = favoriteChampions[key].filter(e => {
-                            return e !== championsToDelete[i].champion
-                        })
-                    }
-                }
-            }
-        }
-        const newFavorites = Object.assign({}, favoriteChampions, deleted)
-
-        const {jg, top, adc, sup, mid} = newFavorites
-
-        await firebase.firestore()
-            .collection('users')
-            .doc(currentUser.uid)
-            .update({'favorites': newFavorites})
-
-        setData([
-            ...flatten(top, 'Top Lane'),
-            ...flatten(jg, 'Jungle'),
-            ...flatten(adc, 'Bot Lane'),
-            ...flatten(mid, 'Mid Lane'),
-            ...flatten(sup, 'Support'),
-        ])
-
-        store.addNotification({
-            title: "Champion Deleted!",
-            message: " ",
-            type: "danger",
-            insert: "top",
-            container: "top-right",
-            animationIn: ["animate__animated", "animate__fadeIn"],
-            animationOut: ["animate__animated", "animate__fadeOut"],
-            dismiss: {
-                duration: 5000
-            }
-        });
-
+      }
     }
+    const newFavorites = Object.assign({}, favoriteChampions, deleted);
 
+    const { jg, top, adc, sup, mid } = newFavorites;
 
-    return (
-        <div style={{height: 400, width: 800}}>
+    await firebase
+      .firestore()
+      .collection("users")
+      .doc(currentUser.uid)
+      .update({ favorites: newFavorites });
 
-                <DataGrid
-                    rows={data}
-                    columns={columns}
-                    pageSize={5}
-                    checkboxSelection={true}
-                    onSelectionModelChange={e => selectItems(e.selectionModel)}
-                />
+    setData([
+      ...flatten(top, "Top Lane"),
+      ...flatten(jg, "Jungle"),
+      ...flatten(adc, "Bot Lane"),
+      ...flatten(mid, "Mid Lane"),
+      ...flatten(sup, "Support"),
+    ]);
 
+    store.addNotification({
+      title: "Champion Deleted!",
+      message: " ",
+      type: "danger",
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animate__animated", "animate__fadeIn"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 5000,
+      },
+    });
+  };
 
-            <Button
-                variant="contained"
-                color="primary"
-                className="login-form__smallButton"
-                onClick={() => handleDelete()}
-            >
-                Delete Selected
-            </Button>
-        </div>
-    );
+  return (
+    <div style={{ height: 400, width: 800 }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection={true}
+        onSelectionModelChange={(e) => selectItems(e.selectionModel)}
+      />
+
+      <Button
+        variant="contained"
+        color="primary"
+        className="login-form__smallButton"
+        onClick={() => handleDelete()}
+      >
+        Delete Selected
+      </Button>
+    </div>
+  );
 }
--- frontend/src/pages/Stats/components/BarGraphs.js
+++ frontend/src/pages/Stats/components/BarGraphs.js
@@ -1,58 +1,64 @@
-import React from 'react';
-import {Bar, BarChart, Tooltip, XAxis, YAxis} from "recharts";
+import React from "react";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 import flatten from "./Flatten";
-import {DefaultTooltipContent} from 'recharts/lib/component/DefaultTooltipContent';
+import { DefaultTooltipContent } from "recharts/lib/component/DefaultTooltipContent";
 
-const convertPickBan = (name)=>{
-    return name.includes('bans')? 'Bans' : 'Picks'
-}
-
-const CustomTooltip = props => {
-    if (!props.active) {
-        return null
-    }
-
-    if (props.payload === undefined){
-        return null
-    }
-
-    const champ  = props.label
-    const bans = props.payload[0].value
-    const banOrPick = props.payload[0].name
-
-    const newPayload = [
-        {
-            'name': 'Champion',
-            'value': champ,
-        },
-        {
-            'name': convertPickBan(banOrPick),
-            'value': bans
-        }
-    ];
-
-    return <DefaultTooltipContent payload={newPayload}/>;
+const convertPickBan = (name) => {
+  return name.includes("bans") ? "Bans" : "Picks";
 };
 
-export default function BarGraph({data}) {
-    return <div style={{display:"flex", flexDirection: "column", alignItems: "center"}}>
-        <h2>{data.title}</h2>
-        <BarChart
-            width={600}
-            height={300}
-            data={flatten(data)}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}
-            title={data.title}
-        >
-            <XAxis
-                dataKey="index"
-                stroke="white"
-                tick={false}
-            />
+const CustomTooltip = (props) => {
+  if (!props.active) {
+    return null;
+  }
 
-            <YAxis stroke="white"/>
-            <Bar dataKey="value" fill="#c79b3b" name={data.tooltip} />
-            <Tooltip content={<CustomTooltip/>}/>
-        </BarChart>
+  if (props.payload === undefined) {
+    return null;
+  }
+
+  const champ = props.label;
+  const bans = props.payload[0].value;
+  const banOrPick = props.payload[0].name;
+
+  const newPayload = [
+    {
+      name: "Champion",
+      value: champ,
+    },
+    {
+      name: convertPickBan(banOrPick),
+      value: bans,
+    },
+  ];
+
+  return <DefaultTooltipContent payload={newPayload} />;
+};
+
+export default function BarGraph({ data }) {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h2>{data.title}</h2>
+      <BarChart
+        width={600}
+        height={300}
+        data={flatten(data)}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        title={data.title}
+      >
+        <XAxis
+          dataKey="index"
+          stroke="white"
+          angle={-45}
+          textAnchor="end"
+          interval={0}
+        />
+
+        <YAxis stroke="white" />
+        <Bar dataKey="value" fill="#c79b3b" name={data.tooltip} />
+        <Tooltip content={<CustomTooltip />} />
+      </BarChart>
     </div>
+  );
 }
