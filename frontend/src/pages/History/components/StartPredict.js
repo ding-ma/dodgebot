e3c82107543f2bd161f5fdb7d756943b90d5d9d0
Petar Basta <petar.basta@hotmail.com>
2021-03-28 20:26:39 -0400

    Fix lint

--- frontend/src/pages/History/components/StartPredict.js
+++ frontend/src/pages/History/components/StartPredict.js
@@ -1,31 +1,8 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import {Button} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
 
-
-const useStyles = makeStyles({
-    root: {
-        minWidth: 800,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-});
-
-
 export default function StartPredict() {
-    const classes = useStyles();
     const history = useHistory();
 
     return (
