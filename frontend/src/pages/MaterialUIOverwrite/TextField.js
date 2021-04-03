0cb7c04ecf25a0c7cd6c2a6979b1c18ec5f1640a
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2021-04-03 11:57:28 -0400

    removed autocomplete

--- frontend/src/pages/MaterialUIOverwrite/TextField.js
+++ frontend/src/pages/MaterialUIOverwrite/TextField.js
@@ -1,79 +1,80 @@
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import TextFieldMui from "@material-ui/core/TextField";
 import React from "react";
 
-const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
+const styles = (theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
 
-    label: {
-        color : '#c79b3b',
-        "&$focusedLabel": {
-            color: "#c79b3b"
-        },
-        "&$erroredLabel": {
-            color: "red"
-        },
-        "&.Mui-focused": {
-            color: "#c79b3b"
-        }
+  label: {
+    color: "#c79b3b",
+    "&$focusedLabel": {
+      color: "#c79b3b",
     },
+    "&$erroredLabel": {
+      color: "red",
+    },
+    "&.Mui-focused": {
+      color: "#c79b3b",
+    },
+  },
 
-    cssOutlinedInput: {
-        color: "white",
-        '&$cssFocused $notchedOutline': {
-            borderColor: '#c79b3b !important',
-        },
+  cssOutlinedInput: {
+    color: "white",
+    "&$cssFocused $notchedOutline": {
+      borderColor: "#c79b3b !important",
     },
+  },
 
-    cssFocused: {
-        borderWidth: '1px',
-        borderColor: '#c79b3b !important'
-    },
+  cssFocused: {
+    borderWidth: "1px",
+    borderColor: "#c79b3b !important",
+  },
 
-    notchedOutline: {
-        borderWidth: '1px',
-        borderColor: '#c79b3b !important'
-    },
-    disabledInput: {
-        color: '#c4bb97',
-    },
-    helper: {
-        color: 'white'
-    },
-    icon: {
-        color: '#c79b3b',
-    }
+  notchedOutline: {
+    borderWidth: "1px",
+    borderColor: "#c79b3b !important",
+  },
+  disabledInput: {
+    color: "#c4bb97",
+  },
+  helper: {
+    color: "white",
+  },
+  icon: {
+    color: "#c79b3b",
+  },
 });
 
-const TextField = withStyles(styles)(function TextField({classes, ...props}) {
-    return (
-        <TextFieldMui
-            InputLabelProps={{
-                classes: {
-                    root: classes.label,
-                    focused: classes.cssFocused,
-                },
-            }}
-            InputProps={{
-                classes: {
-                    root: classes.cssOutlinedInput,
-                    focused: classes.cssFocused,
-                    notchedOutline: classes.notchedOutline,
-                    disabled: classes.disabledInput
-                },
-            }}
-            SelectProps={{
-                classes: {
-                    icon: classes.icon
-                }
-            }}
-            FormHelperTextProps={{ classes: { root: classes.helper } }}
-            {...props}
-        />
-    );
+const TextField = withStyles(styles)(function TextField({ classes, ...props }) {
+  return (
+    <TextFieldMui
+      autoComplete="no"
+      InputLabelProps={{
+        classes: {
+          root: classes.label,
+          focused: classes.cssFocused,
+        },
+      }}
+      InputProps={{
+        classes: {
+          root: classes.cssOutlinedInput,
+          focused: classes.cssFocused,
+          notchedOutline: classes.notchedOutline,
+          disabled: classes.disabledInput,
+        },
+      }}
+      SelectProps={{
+        classes: {
+          icon: classes.icon,
+        },
+      }}
+      FormHelperTextProps={{ classes: { root: classes.helper } }}
+      {...props}
+    />
+  );
 });
 
 export default withStyles(styles)(TextField);
--- frontend/src/pages/Register/components/RegisterForm.js
+++ frontend/src/pages/Register/components/RegisterForm.js
@@ -1,105 +1,105 @@
-import React, {useState} from 'react';
-import firebase from 'firebase';
-import {useHistory} from 'react-router-dom';
-import {Button, Typography,} from '@material-ui/core';
-import TextField from "../../MaterialUIOverwrite/TextField"
+import React, { useState } from "react";
+import firebase from "firebase";
+import { useHistory } from "react-router-dom";
+import { Button, Typography } from "@material-ui/core";
+import TextField from "../../MaterialUIOverwrite/TextField";
 
 const RegisterForm = () => {
-    const history = useHistory();
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirm, setConfirm] = useState('');
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
-    const [confirmError, setConfirmError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const handleSubmit = async () => {
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const emailCheck = email.length && email.match(emailRegex);
-        const passwordCheck = password.length >= 8;
-        const confirmCheck = password === confirm && confirm.length;
-        
-        if (emailCheck && passwordCheck && confirmCheck) {
-            try {
-                await firebase.auth().createUserWithEmailAndPassword(email, password);
-                setEmailError(false);
-                setPasswordError(false);
-                setConfirmError(false);
-                setErrorMessage('');
-                history.push('/new');
-            } catch (err) {
-                switch (err.code) {
-                    case 'auth/email-already-in-use':
-                        setEmailError(true);
-                        setPasswordError(false);
-                        setConfirmError(false);
-                        setErrorMessage('This email is already in use!');
-                        break;
-                    default:
-                        console.log(err.message);
-                }
-            }
-        } else {
-            setEmailError(!emailCheck);
-            setPasswordError(!passwordCheck);
-            setConfirmError(!confirmCheck);
-            setErrorMessage('One or more fields have an error!');
+  const history = useHistory();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmError, setConfirmError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async () => {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailCheck = email.length && email.match(emailRegex);
+    const passwordCheck = password.length >= 8;
+    const confirmCheck = password === confirm && confirm.length;
+
+    if (emailCheck && passwordCheck && confirmCheck) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+        setEmailError(false);
+        setPasswordError(false);
+        setConfirmError(false);
+        setErrorMessage("");
+        history.push("/new");
+      } catch (err) {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+            setEmailError(true);
+            setPasswordError(false);
+            setConfirmError(false);
+            setErrorMessage("This email is already in use!");
+            break;
+          default:
+            console.log(err.message);
         }
-    };
-    
-    return (
-        <form className="login-form">
-            {errorMessage && (
-                <Typography className="login-form__error" color="error">
-                    {errorMessage}
-                </Typography>
-            )}
-            <TextField
-                required
-                label="Email address"
-                value={email}
-                onChange={(e) => setEmail(e.currentTarget.value)}
-                className="login-form__input"
-                variant="outlined"
-                color="primary"
-                error={emailError}
-            />
-            
-            <TextField
-                required
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.currentTarget.value)}
-                className="login-form__input"
-                variant="outlined"
-                color="primary"
-                helperText="Password must be at least 8 characters long"
-                error={passwordError}
-            />
-            <TextField
-                required
-                label="Confirm password"
-                type="password"
-                value={confirm}
-                onChange={(e) => setConfirm(e.currentTarget.value)}
-                className="login-form__input"
-                variant="outlined"
-                color="primary"
-                error={confirmError}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                className="login-form__button"
-                onClick={() => handleSubmit()}
-            >
-                Sign up
-            </Button>
-        </form>
-    );
+      }
+    } else {
+      setEmailError(!emailCheck);
+      setPasswordError(!passwordCheck);
+      setConfirmError(!confirmCheck);
+      setErrorMessage("One or more fields have an error!");
+    }
+  };
+
+  return (
+    <form className="login-form">
+      {errorMessage && (
+        <Typography className="login-form__error" color="error">
+          {errorMessage}
+        </Typography>
+      )}
+      <TextField
+        required
+        label="Email address"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        className="login-form__input"
+        variant="outlined"
+        color="primary"
+        error={emailError}
+      />
+
+      <TextField
+        required
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        className="login-form__input"
+        variant="outlined"
+        color="primary"
+        helperText="Password must be at least 8 characters long"
+        error={passwordError}
+      />
+      <TextField
+        required
+        label="Confirm password"
+        type="password"
+        value={confirm}
+        onChange={(e) => setConfirm(e.currentTarget.value)}
+        className="login-form__input"
+        variant="outlined"
+        color="primary"
+        error={confirmError}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        className="login-form__button"
+        onClick={() => handleSubmit()}
+      >
+        Sign up
+      </Button>
+    </form>
+  );
 };
 
 export default RegisterForm;
